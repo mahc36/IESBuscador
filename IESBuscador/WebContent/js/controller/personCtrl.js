d790f30f771e013985690149482eae67ae6bd26c
programador <programador@10.100.98.107>
2017-06-07 17:20:25 -0500

    Cerrar sesión

--- IESBuscador/WebContent/js/controller/personCtrl.js
+++ IESBuscador/WebContent/js/controller/personCtrl.js
@@ -1,4 +1,4 @@
-app.controller("personCtrl", ["$scope","personService","$cookieStore", function personCtrl($scope, personService, $cookieStore) {
+app.controller("personCtrl", ["$scope","personService","$cookieStore","$window", function personCtrl($scope, personService, $cookieStore,$window) {
 	
 	//Register
 	$scope.personForm={};
@@ -24,7 +24,9 @@ app.controller("personCtrl", ["$scope","personService","$cookieStore", function 
 				person={"id":personForm.id,"name": personForm.name,"email":personForm.email,"password":personForm.password};
 				personService.addPerson(person).then(function(message){
 					if(message.data == "OK"){
-						alert("Aquí debemos de ir a la pagina principal");
+						alert("Registrado con éxito");
+						personForLogin={"email":personForm.email,"password":personForm.password};
+						$scope.logIn(personForLogin);
 					}
 					else if(message.data == "EMAILERROR"){
 						alert("El correo ya esta en uso");
@@ -38,15 +40,56 @@ app.controller("personCtrl", ["$scope","personService","$cookieStore", function 
 	}
 	
 	$scope.logOut=function(){
-		alert("vamos a cerrar la sesión");
 		var getCookie = $cookieStore.get('cookiePersonSession');
 		alert("get cookie: "+getCookie);
 		if(!angular.isUndefined(getCookie)){
 			alert("Hasta luego "+getCookie.name);
 			$cookieStore.remove('cookiePersonSession');
 		}
+		else{
+			alert("No hay ninguna sesión");
+		}
 	}
-		
+	
+	//Login
+	$scope.personFormLogin={};
+	$scope.logIn=function(personFormLogin){
+		var getCookie = $cookieStore.get('cookiePersonSession');
+		if(!angular.isUndefined(getCookie)){
+			alert("Ahora mismo hay una sesión activa");
+			return;
+		}
+		var personSession={};
+		if(angular.isUndefined(personFormLogin.email) || angular.isUndefined(personFormLogin.password)){
+			alert("Completa todos los campos");
+			return;
+		}
+		else{
+			personService.findByEmail(personFormLogin.email).then(function(message){
+				if(message.data.id == 0 ||message.data.name == null || message.data.email == null || message.data.password == null){
+					alert("No reconocemos ese correo");
+				}
+				else{
+					if(message.data.password == personFormLogin.password){
+						personSession.id = message.data.id;
+						personSession.name = message.data.name;
+						personSession.email= message.data.email;
+						personSession.password = message.data.password;
+						$cookieStore.put('cookiePersonSession',personSession);
+						var getCookie = $cookieStore.get('cookiePersonSession');
+						alert("Bienvenido "+getCookie.name);
+						$window.location.href = '../index.html';
+					}
+					else{
+						alert("El correo y/o la contraseña están incorrectas");
+					}
+				}
+			})
+		}
+	}
+	
+	
+	
 	$scope.checkCookie=function(){
 //		var getCookie = $cookieStore.get('cookiePersonSession');
 //		alert("El hijodeputa nombre es: "+getCookie.name);
@@ -60,37 +103,5 @@ app.controller("personCtrl", ["$scope","personService","$cookieStore", function 
 //		}
 //		alert("contador "+contador);
 //		contador=contador+1;
-	}
-	
-	//Login
-	$scope.personFormLogin={};
-	$scope.logIn=function(personFormLogin){
-		var personSession={};
-		if(angular.isUndefined(personFormLogin.email) || angular.isUndefined(personFormLogin.password)){
-			alert("Completa todos los campos");
-			return;
-		}
-		else{
-			personService.findByEmail(personFormLogin.email).then(function(message){
-				if(message.data.id == 0 ||message.data.name == null || message.data.email == null || message.data.password == null){
-					alert("No reconocemos ese correo");
-				}
-				else{
-					if(message.data.password == personFormLogin.password){
-						alert("Dio todo bien y ya vamos a insertar en la cookie");
-						personSession.id = message.data.id;
-						personSession.name = message.data.name;
-						personSession.email= message.data.email;
-						personSession.password = message.data.password;
-						$cookieStore.put('cookiePersonSession',personSession);
-						var getCookie = $cookieStore.get('cookiePersonSession');
-						alert("Bienvenido "+getCookie.name);
-					}
-					else{
-						alert("El correo y/o la contraseña están incorrectas");
-					}
-				}
-			})
-		}
 	}
 }]);
